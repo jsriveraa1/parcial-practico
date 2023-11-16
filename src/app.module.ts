6c309ecd347fa34c68b54e92a606aee134ef1e63
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2023-11-16 13:46:19 -0500

    persistencia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductoModule } from './producto/producto.module';
+import { TiendaModule } from './tienda/tienda.module';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
+import { ProductoEntity } from './producto/producto.entity/producto.entity';
+import { TiendaEntity } from './tienda/tienda.entity/tienda.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductoModule,
+    TiendaModule,
+    TypeOrmCoreModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial',
+      entities: [ProductoEntity, TiendaEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/tienda/tienda.entity/tienda.entity.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductoEntity } from "../../producto/producto.entity/producto.entity";
+
+@Entity()
+export class TiendaEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  ciudad: string;
+
+  @Column()
+  direccion: string;
+
+  @ManyToMany(() => ProductoEntity, (producto) => producto.tiendas)
+  @JoinTable()
+  productos: ProductoEntity[];
+
+}
